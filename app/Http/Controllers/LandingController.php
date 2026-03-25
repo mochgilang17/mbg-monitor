96a6de97cbfe0e949d05a153e8a18c9e5ad80b36
mochgilang17 <gilangelangperkasa@.com>
2026-03-25 20:10:33 +0800

    add vercel config

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
-    //
+    public function index()
+    {
+        // Nantinya kita akan ambil data dari database di sini
+        // Untuk sekarang, kita panggil view 'welcome' saja
+        return view('welcome');
+    }
 }
